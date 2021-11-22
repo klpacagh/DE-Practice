4e22924fd05edffb8ebcf6ce940d942ed30f778d
Kevin Le <pandaboi35@yahoo.com>
2021-11-21 20:34:26 -0500

    YE

---
+++ ETL-Spotify/payload/config/playlists.py
@@ -0,0 +1,8 @@
+def spotify_playlists():
+    playlists = {'drum_and_bass_2021': 'spotify:playlist:068WHS0zOWsqvn2uIBYb5D'}
+    return playlists
+
+
+def personal_playlists():
+    playlists = {'drive_hood': 'spotify:playlist:1uM1laLmdeCMRQoYC1gybB'}
+    return playlists
